10d82d847f12b489c4aaf9336786725edbd79467
Sandesh <ksandesh283@gmail.com>
2023-02-24 14:44:57 +0545

    removed splash screen package which affected the flutter build method in actions

--- lib/Screens/S.dart
+++ lib/Screens/S.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class S extends StatefulWidget {
   const S({super.key});
